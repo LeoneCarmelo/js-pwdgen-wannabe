058bcc5afc35e5360c4ef452831c1cef9891f127
LeoneCarmelo <carmeloleone@outlook.it>
2023-03-07 17:09:52 +0100

    Replace let with const

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,11 @@
 
 // Chiedi all'utente il suo nome,
-let name = prompt('Ciao, per generare la tua password inserisci il tuo nome')
+const name = prompt('Ciao, per generare la tua password inserisci il tuo nome')
 // poi chiedi il suo cognome,
-let surname = prompt('Adesso inserisci il tuo cognome')
+const surname = prompt('Adesso inserisci il tuo cognome')
 // poi chiedi il suo colore preferito
-let color = prompt('Infine inserisci il tuo colore preferito')
+const color = prompt('Infine inserisci il tuo colore preferito')
 // Infine scrivi sulla pagina il risultato usando questo formato: nomecognomecolorepreferito23
-let resultEl = document.getElementById('result')
+const resultEl = document.getElementById('result')
 
 resultEl.innerHTML = `La tua nuova password è: ${name}${surname}${color}23`
